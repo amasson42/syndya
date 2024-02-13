a79adb8fabbc39deee7795089d7bd187ade4bafb
amasson42 <giantwow3896@gmail.com>
2024-02-13 11:14:53 +0100

    protect connections map with mutex

--- docs/API v1.md
+++ docs/API v1.md
@@ -9,5 +9,14 @@ This document outlines the endpoints and functionalities provided by the API.
 - **Description**: Subscribe to look for a game.
 - A web socket connection is established
 
+*Server messages*
+- `id <playerId>` : Send the player searching id
+- `meta <dataKey>` : Kindly ask the user to send the value of the metadata
+- `game <gameInfo>` : Send the playable game informations to the player
+
+*Client messages*
+- `id` : Kindly request the server to send out searching id
+- `meta <string>` : Send to the server a metadata with a key and a value in the format `key=value`
+
 > `GET /players`
 - **Description**: Get the list of all searching players
--- internal/Controllers/PlayersController.go
+++ internal/Controllers/PlayersController.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"syndya/pkg/Models"
 
 	"github.com/gin-gonic/gin"
@@ -12,9 +13,10 @@ import (
 )
 
 type PlayersController struct {
-	upgrader    websocket.Upgrader
-	playersBank Models.SearchingPlayersBank
-	openSockets map[int]*websocket.Conn
+	upgrader         websocket.Upgrader
+	playersBank      Models.SearchingPlayersBank
+	connections      map[int]*websocket.Conn
+	connectionsMutex sync.Mutex
 }
 
 func NewPlayersController(playersBank Models.SearchingPlayersBank) *PlayersController {
@@ -24,7 +26,7 @@ func NewPlayersController(playersBank Models.SearchingPlayersBank) *PlayersContr
 			WriteBufferSize: 1024,
 		},
 		playersBank: playersBank,
-		openSockets: map[int]*websocket.Conn{},
+		connections: map[int]*websocket.Conn{},
 	}
 	return &controller
 }
@@ -44,21 +46,24 @@ func (controller *PlayersController) searchGame(c *gin.Context) {
 
 	playerId := controller.playersBank.CreateSearchingPlayer()
 
-	controller.openSockets[playerId] = conn
+	controller.connectionsMutex.Lock()
+	controller.connections[playerId] = conn
+	controller.connectionsMutex.Unlock()
 
 	conn.SetCloseHandler(func(code int, text string) error {
 		fmt.Printf("Closed connection with %v", playerId)
 		return nil
 	})
-	defer conn.Close()
-	defer delete(controller.openSockets, playerId)
-	defer controller.playersBank.DeleteSearchingPlayer(playerId)
 
-	err = conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("id %v", playerId)))
-	if err != nil {
-		log.Println("Error writing message: ", err)
-		return
-	}
+	defer func() {
+		controller.playersBank.DeleteSearchingPlayer(playerId)
+		controller.connectionsMutex.Lock()
+		delete(controller.connections, playerId)
+		controller.connectionsMutex.Unlock()
+		conn.Close()
+	}()
+
+	controller.sendPlayerId(playerId)
 
 	for {
 		_, message, err := conn.ReadMessage()
@@ -68,28 +73,42 @@ func (controller *PlayersController) searchGame(c *gin.Context) {
 			break
 		}
 
-		cmd := strings.SplitN(string(message[:]), " ", 2)
+		controller.interpretWebSocketMessage(string(message), playerId)
 
-		if len(cmd) >= 1 {
-			switch cmd[0] {
-			case "meta":
-				if len(cmd) >= 2 {
-					parameters := strings.SplitN(cmd[1], "=", 2)
-					if len(parameters) == 2 {
-						metaKey := parameters[0]
-						metaValue := parameters[1]
-						controller.playersBank.UpdateSearchingPlayerMetadata(playerId, metaKey, metaValue)
-					}
+	}
+}
+
+func (controller *PlayersController) interpretWebSocketMessage(message string, playerId int) {
+	cmd := strings.SplitN(string(message[:]), " ", 2)
+
+	if len(cmd) >= 1 {
+		switch cmd[0] {
+		case "meta":
+			if len(cmd) >= 2 {
+				parameters := strings.SplitN(cmd[1], "=", 2)
+				if len(parameters) == 2 {
+					metaKey := parameters[0]
+					metaValue := parameters[1]
+					controller.playersBank.UpdateSearchingPlayerMetadata(playerId, metaKey, metaValue)
 				}
 			}
+		case "id":
+			controller.sendPlayerId(playerId)
 		}
+	}
+}
 
-		// err = conn.WriteMessage(websocket.TextMessage, []byte("Hello, socket!"))
-		// if err != nil {
-		// 	log.Println("Error writing message: ", err)
-		// 	break
-		// }
-
+func (controller *PlayersController) sendPlayerId(playerId int) {
+	controller.connectionsMutex.Lock()
+	conn := controller.connections[playerId]
+	controller.connectionsMutex.Unlock()
+	if conn == nil {
+		return
+	}
+	err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("id %v", playerId)))
+	if err != nil {
+		log.Println("Error writing message: ", err)
+		return
 	}
 }
 
